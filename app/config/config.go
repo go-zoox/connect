e1232702ef9198f0cc378b70e6dbba70b7dac774
Zero <tobewhatwewant@outlook.com>
2022-08-17 07:29:22 +0000

    fix: config

--- app/config/config.go
+++ app/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"errors"
+	"fmt"
 	"net/url"
 	"os"
 	"strconv"
@@ -178,6 +179,60 @@ func Load(config_file string) (*Config, error) {
 	return &cfg, nil
 }
 
+func LoadFromService(fn func() (string, error)) (*Config, error) {
+	var cfg Config
+	if err := goconfig.LoadFromService(&cfg, fn); err != nil {
+		return nil, fmt.Errorf("load config from service error: %s", err)
+	}
+
+	if cfg.SecretKey == "" {
+		return nil, errors.New("secret_key is empty")
+	}
+
+	if cfg.SessionMaxAge == 0 {
+		// cfg.SessionMaxAge = 7200 * 1000 // 2 hours
+		cfg.SessionMaxAgeDuration = 2 * time.Hour
+	} else {
+		cfg.SessionMaxAgeDuration = time.Duration(cfg.SessionMaxAge) * time.Millisecond
+	}
+
+	if cfg.Port == 0 {
+		cfg.Port = 8080
+	}
+	if cfg.LogLevel == "" {
+		cfg.LogLevel = "ERROR"
+	}
+	if cfg.Mode == "" {
+		cfg.Mode = "development"
+	}
+
+	// default auth mode => oauth2
+	if cfg.Auth.Mode == "" {
+		cfg.Auth.Mode = "oauth2"
+	}
+	// default oauth2 provider => doreamon
+	if cfg.Auth.Provider == "" {
+		cfg.Auth.Provider = "doreamon"
+	}
+
+	// default services mode => service
+	if cfg.Services.App.Mode == "" {
+		cfg.Services.App.Mode = "service"
+	}
+	if cfg.Services.User.Mode == "" {
+		cfg.Services.User.Mode = "service"
+	}
+	if cfg.Services.Menus.Mode == "" {
+		cfg.Services.Menus.Mode = "service"
+	}
+
+	applyEnv()
+
+	// isLoaded = true
+
+	return &cfg, nil
+}
+
 func applyEnv() {
 	if os.Getenv("PORT") != "" {
 		v, err := strconv.Atoi(os.Getenv("PORT"))
@@ -236,5 +291,4 @@ func applyEnv() {
 			Port:   int64(port),
 		}
 	}
-
 }
